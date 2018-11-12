6bf6c72a80ed913eb51ef4d47f8973dced0276e9
Alexander Morton <mortonprod@gmail.com>
2018-11-12 22:32:10 +0000

    Add: Simple server

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
-
-import "fmt"
-
+import (
+  "net/http"
+  "strings"
+)
+func sayHello(w http.ResponseWriter, r *http.Request) {
+  message := r.URL.Path
+  message = strings.TrimPrefix(message, "/")
+  message = "Hello " + message
+  w.Write([]byte(message))
+}
 func main() {
-    fmt.Println("Hello World")
+  http.HandleFunc("/", sayHello)
+  if err := http.ListenAndServe(":8080", nil); err != nil {
+    panic(err)
+  }
 }
